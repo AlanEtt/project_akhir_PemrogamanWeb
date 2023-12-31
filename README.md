47f6faa7fdc58094e153cd640dcf2aa138bdb3b9
AlanEtt <alanetkaji2@gmail.com>
2023-12-31 21:59:18 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Project Akhir Pemrogaman Web
+- NAMA  :  RIZKI ALAN HABIBI
+- NIM   : 221240001238
---
+++ menu.php
@@ -0,0 +1,33 @@
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+            <a class="navbar-brand" href="home.php" style="padding-left: 100px;">Home</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" style="padding-left: 10px;" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="total_village.php">Total Kelurahan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="user.php">Pengguna</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="change_password.php">Ganti Password</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="chained_dropdown.php">Pilihan Bertingkat</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="religion.php">Agama</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="marital.php">Status Perkawinan</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
